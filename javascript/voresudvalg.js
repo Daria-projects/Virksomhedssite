bfb0bab8ade272c2e4695ad25acaed01d8052cb7
Julie Jacobsen <juja1000@stud.ek.dk>
2026-05-20 13:21:49 +0200

    prøve på fold ud menu

--- javascript/voresudvalg.js
+++ javascript/voresudvalg.js
@@ -1,1 +1,6 @@
-.laegikurv_blaabuket{}
+// const dropdown = document.querySelector(".dropdown");
+// const button = document.querySelector(".dropdown-btn");
+
+// button.addEventListener("click", () => {
+//   dropdown.classList.toggle("open");
+// });
